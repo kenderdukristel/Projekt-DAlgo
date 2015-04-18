a2a0316a4520145347e64d3749e17be149ec5fe4
Finn Bayer <finn.bayer@student.jade-hs.de>
2015-04-18 18:06:34 +0200

    testscript for openFolder.m

---
+++ src/code/openFolderTest.m
@@ -0,0 +1,16 @@
+% Script to test the function openFolder.m 
+% Author: Finn Bayer, Kristin Haßelbusch, Sandro Wehrhahn (c) TGM @ Jade Hochschule applied licence see EOF 
+% Version History:
+% Ver. 0.01 initial create (empty) 18-Apr-2015 			 Initials (eg. JB)
+
+clear;
+close all;
+clc;
+
+%------------Script implementation-------- 
+%test foldername
+folderdir='test';
+
+openFolder(folderdir);
+
+
