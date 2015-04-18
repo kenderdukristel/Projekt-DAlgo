4284a9d236062af1694839455d662c7410d6e674
Finn Bayer <finn.bayer@student.jade-hs.de>
2015-04-18 18:05:11 +0200

    removed the license part of the code

--- createMatlab/DAlgoFunction.m
+++ createMatlab/DAlgoFunction.m
@@ -154,14 +154,6 @@ fprintf(fid, '\n');
 fprintf(fid, '\n');
 fprintf(fid, '\n');
 fprintf(fid, '\n');
-fprintf(fid, '%%--------------------Licence ---------------------------------------------\n');
-szYear = date;
-szYear(1:end-4) = [];
-fprintf(fid, '%% Copyright (c) <%s> %s\n',szYear,szAuthor);
-fprintf(fid, '%% Jade University of Applied Sciences \n');
-fprintf(fid, '%% Permission is hereby granted, free of charge, to any person obtaining \n%% a copy of this software and associated documentation files \n%% (the "Software"), to deal in the Software without restriction, including \n%% without limitation the rights to use, copy, modify, merge, publish, \n%% distribute, sublicense, and/or sell copies of the Software, and to\n%% permit persons to whom the Software is furnished to do so, subject\n%% to the following conditions:\n');
-fprintf(fid, '%% The above copyright notice and this permission notice shall be included \n%% in all copies or substantial portions of the Software.\n');
-fprintf(fid, '%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, \n%% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES \n%% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. \n%% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY \n%% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, \n%% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE \n%% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.');
 
 fclose (fid);
 if bGenTestScript
