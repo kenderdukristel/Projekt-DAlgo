7906b293634eade65b01bd91c166c16ca1437a13
Finn Bayer <finn.bayer@student.jade-hs.de>
2015-04-18 16:34:10 +0200

    updated the implementation

---
+++ src/code/playFile.m
@@ -0,0 +1,18 @@
+function []=playFile(filedir)
+% function to do something usefull (fill out)
+% Usage []=playFile(filedir)
+% Input Parameter:
+%	 filedir:	Explain the parameter, default values, and units
+
+%------------------------------------------------------------------------ 
+% Example: Provide example here if applicable (one or two lines) 
+
+% Author: Finn Bayer, Kristin Haßelbusch, Sandro Wehrhahn (c) TGM @ Jade Hochschule applied licence see EOF 
+% Source: If the function is based on a scientific paper or a web site, 
+%         provide the citation detail here (with equation no. if applicable)  
+% Version History:
+% Ver. 0.01 initial create (empty) 18-Apr-2015  Initials (eg. JB)
+
+%------------Function implementation--------------------------- 
+
+
