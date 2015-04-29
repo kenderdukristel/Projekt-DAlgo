8fdb6ef6e4545d2c7cf17ebcbea29c39ee8299a4
Finn Bayer <finn.bayer@student.jade-hs.de>
2015-04-29 18:43:02 +0200

    improved search algorithm; edited doc

--- src/code/fileSearchPar.m
+++ src/code/fileSearchPar.m
@@ -4,7 +4,7 @@ function fileListPar = fileSearchPar(fileCell,par,partString)
 % Input Parameter:
 %	 fileCell: 		cell array containing content of file in strings for
 %                   each line
-%	 par:           string to search for, give 0 for all data
+%	 par:           searchstring, if zero, fileSearchPar returns all filenames
 %    partString:    number that specifies the part of the line in which to
 %                   search
 % Output Parameter:
@@ -13,15 +13,18 @@ function fileListPar = fileSearchPar(fileCell,par,partString)
 %------------------------------------------------------------------------
 % Example: fileListPar = fileSearchPar(senFile,'cpm0',1)
 
-% Author: Finn Bayer, Kristin Haßelbusch, Sandro Wehrhahn (c) TGM @ Jade Hochschule applied licence see EOF
+% Author: Finn Bayer, Kristin Hasselbusch, Sandro Wehrhahn (c) TGM @ Jade Hochschule applied licence see EOF
 
 % Version History:
-% Ver. 0.01 initial create (empty) 18-Apr-2015  KH
-% Ver. 1.00 first implementation 18-Apr-2015  KH
-% Ver. 1.10 added for-loop to ignore filename for search 25-Apr-2015  KH
-% Ver. 1.20 added new argument, for-loop modified 27-Apr-2015  KH
+% Ver. 0.01 initial create (empty) 18-Apr-2015  (FB,KH,SW)
+% Ver. 1.00 first implementation 18-Apr-2015  (FB,KH,SW)
+% Ver. 1.10 added for-loop to ignore filename for search 25-Apr-2015  (FB,KH,SW)
+% Ver. 1.20 added new argument, for-loop modified 27-Apr-2015  (FB,KH,SW)
+% Ver. 2.00 improved the search algorithm 29-Apr-2015 (FB,KH,SW)
+
 
 %------------Function implementation---------------------------
+
 
 %vec contains all strings of the lines
 vec = fileCell{1};
@@ -37,23 +40,54 @@ end
 %search for par in fileCell
 fileListPar = {};
 if par~=0
-    %split line by tab and only save first or second (specified by
-    %partString) part of the line in vec
-    for k = 1:length(vec)
-        lineSplitted = strsplit(vec{k},'\t');
-        vec{k} = lineSplitted{partString};
+    
+    %%%%%%%%%%% Editing String to improve search algorithm %%%%%%%%%%%%%%%%
+    
+    %to avoid case sensivity
+    par = lower(par);
+    if partString == 2
+        %add spaces to the search string to avoid getting matches for
+        %only parts of the word
+        par = [' ' par ' '];
+        %delete commata and dots to recognize words near the punctuations
+        par = strrep(par,',','');
+        par = strrep(par,'.','');
     end
-    %search in the cell array vec after the string par
-    %fileListPar is cell array containing all filenames where par is found
-    for i=1:length(vec)
-        s = strfind(vec{i},par); %search for par in one line
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %%%%%%%%%%%%%%% Search algorithm %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    for k = 1:length(vec)
+        
+        %split line by tab to seperate into filename and sentence
+        lineSplitted = strsplit(vec{k},'\t');
+        
+        %if searching for the speaker id-> extraction from the filename
+        if partString == 1
+            temp = strsplit(lineSplitted{partString},{'-','/'});
+            lineSplitted{1} = temp{2}(2:end);
+        end
+        
+        %add spaces to the reference string to avoid getting matches for
+        %only parts of the word
+        lineSplitted{partString} = [' ' lineSplitted{partString} ' '];
+        
+        %search in the part of the line (lineSplitted) for the search
+        %string
+        s = strfind(lineSplitted{partString},par);
+        
+        %fileListPar is a cell array containing all filenames where par is found
         if isempty(s)==0
-            newFileName = fileList{i}; %add current filename to list
-            fileListPar = [fileListPar; newFileName];
+            newFileName = fileList{k};
+            fileListPar = [fileListPar; newFileName];%add current filename to list
         end
     end
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
 else
-    %if par is zero fileListPar is cell array with all filenames
+    %if given parameter is not specified, fileListPar is cell array with all filenames
     fileListPar = fileList;
 end
 
