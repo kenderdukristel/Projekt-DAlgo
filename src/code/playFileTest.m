1ecb039d457d4bb79c9cc170fde1af8df1829452
Finn Bayer <finn.bayer@student.jade-hs.de>
2015-04-29 15:58:27 +0200

    updated doc

--- src/code/playFileTest.m
+++ src/code/playFileTest.m
@@ -1,7 +1,7 @@
 % Script to test the function playFile.m 
 % Author: Finn Bayer, Kristin Haßelbusch, Sandro Wehrhahn (c) TGM @ Jade Hochschule applied licence see EOF 
 % Version History:
-% Ver. 0.01 initial create (empty) 18-Apr-2015 			 Initials (eg. JB)
+% Ver. 0.01 initial create (empty) 18-Apr-2015 	(FB,KH,SW)
 % Ver. 1.0 code implemented 18-Apr-2015 (FB,KH,SW)
 
 clear;
